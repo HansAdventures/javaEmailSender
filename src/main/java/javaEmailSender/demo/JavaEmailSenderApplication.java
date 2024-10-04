4ad5ac280580f13aaa7615410428a9e9816559ab
HansVercruysse <hans.vercruysse@ugent.be>
2024-10-04 15:14:01 +0200

    Added EmailService and EmailController
    Added env.properties to gitignore

---
+++ src/main/java/javaEmailSender/demo/JavaEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package javaEmailSender.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmailSenderApplication.class, args);
+	}
+
+}
